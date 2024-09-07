94682af40d092e8e5a159e0120fa75ebea9166ad
agorohov <aleksandr.gorohov@gmail.com>
2024-09-07 22:39:47 +0300

    Алгоритм выбора слов учитывая ранг готов

--- src/main/java/com/agorohov/learnirregverbs_bot/repository/LearningStatisticsRepository.java
+++ src/main/java/com/agorohov/learnirregverbs_bot/repository/LearningStatisticsRepository.java
@@ -2,11 +2,11 @@ package com.agorohov.learnirregverbs_bot.repository;
 
 import com.agorohov.learnirregverbs_bot.entity.LearningStatisticsEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 public interface LearningStatisticsRepository extends JpaRepository<LearningStatisticsEntity, Long> {
 
@@ -23,7 +23,7 @@ public interface LearningStatisticsRepository extends JpaRepository<LearningStat
     List<LearningStatisticsEntity> findAllStatisticsByUserChatId(@Param("user_chat_id") Long userChatId);
 
     @Query(value = "SELECT * FROM learning_statistics WHERE user_chat_id = :user_chat_id AND verb_id = :verb_id", nativeQuery = true)
-    LearningStatisticsEntity findByUserChatIdAndVerbId(@Param("user_chat_id") Long userChatId, @Param("verb_id") Integer verbId);
+    Optional<LearningStatisticsEntity> findByUserChatIdAndVerbId(@Param("user_chat_id") Long userChatId, @Param("verb_id") Integer verbId);
 
     @Query(value = "SELECT EXISTS (SELECT 1 FROM learning_statistics WHERE user_chat_id = :user_chat_id AND verb_id = :verb_id)", nativeQuery = true)
     boolean existsByUserChatIdAndVerbId(@Param("user_chat_id") Long userChatId, @Param("verb_id") Integer verbId);
