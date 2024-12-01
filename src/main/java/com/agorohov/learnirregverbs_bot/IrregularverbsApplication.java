5ba7a0892d152bfe6d6fca2d8ae11cd0ebbda4fa
agorohov <aleksandr.gorohov@gmail.com>
2024-12-01 17:02:27 +0300

    Работает при указании имени, токена и владельца напрямую через applications.yaml

--- src/main/java/com/agorohov/learnirregverbs_bot/IrregularverbsApplication.java
+++ src/main/java/com/agorohov/learnirregverbs_bot/IrregularverbsApplication.java
@@ -1,6 +1,5 @@
 package com.agorohov.learnirregverbs_bot;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Slf4j
 public class IrregularverbsApplication {
-    
-    @PostConstruct
-    void print(){
-        log.info("The method with the @PostConstruct annotation worked");
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(IrregularverbsApplication.class, args);
     }
-
 }
